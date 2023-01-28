996306116f3a65ede6afa37006876d956b0f55d1
Manu <mcabral@frba.utn.edu.ar>
2023-01-28 17:11:27 -0300

    refactor: rename middleware

--- src/graphql/middlewares/isLogged.js
+++ src/graphql/middlewares/isLogged.js
@@ -1,4 +1,7 @@
+const errorObject = require('../../utils/error')
+
 module.exports = (resolver) => async (parent, args, context, info) => {
     const { user } = context
+    if (!user) throw new errorObject({ auth: true })
     return resolver(parent, args, context, info)
 }
