4eb9763e9df642a88de92a2754c9cfa0dc009d14
Manu <mcabral@frba.utn.edu.ar>
2023-01-28 18:57:28 -0300

    feat(service): add user update and find by username

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -4,8 +4,11 @@ const { User } = models
 module.exports = {
     create: async (user_payload) => await User.create(user_payload),
     remove: async (id) => await User.findByIdAndRemove(id),
+    update: async (payload) =>
+        await User.findByIdAndUpdate(payload.id, payload, { new: true }),
     find: async (options) => await User.findOne(options),
     findAll: async () => await User.find(),
     findById: async (id) => await User.findById(id),
     findByEmail: async (email) => module.exports.find({ email }),
+    findByUsername: async (username) => module.exports.find({ username }),
 }
