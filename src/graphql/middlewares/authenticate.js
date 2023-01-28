4bf48d89549a1d6f31679c423b4457f2ac677342
Manu <mcabral@frba.utn.edu.ar>
2023-01-28 17:12:04 -0300

    fix(middleware): throw error

--- src/graphql/middlewares/authenticate.js
+++ src/graphql/middlewares/authenticate.js
@@ -1,11 +1,14 @@
 const authController = require('../../controllers/auth.controller')
 
 module.exports = async (context) => {
-    console.info('Authenticating a request', {
-        origin: context.req.headers.origin,
-        referer: context.req.headers.referer,
-        query: context.params.query,
-    })
-    const user = await authController.verify(context.req.headers.auth)
-    return user
+    try {
+        console.info('Authenticating a request', {
+            origin: context.req.headers.origin,
+            referer: context.req.headers.referer,
+            query: context.params.query,
+        })
+        return await authController.verify(context.req.headers.auth)
+    } catch (error) {
+        return null
+    }
 }
