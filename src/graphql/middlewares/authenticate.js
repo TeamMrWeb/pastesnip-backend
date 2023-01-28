c675d6d13b574d3d84d60737c630e6b51b64d50e
Manu <mcabral@frba.utn.edu.ar>
2023-01-28 16:59:09 -0300

    feat(middleware): finish authenticate

--- src/graphql/middlewares/authenticate.js
+++ src/graphql/middlewares/authenticate.js
@@ -1,5 +1,11 @@
-module.exports = (context) => {
-    // const { authorization } = context.request.headers
-    console.log('authenticate called')
-    // return user
+const authController = require('../../controllers/auth.controller')
+
+module.exports = async (context) => {
+    console.info('Authenticating a request', {
+        origin: context.req.headers.origin,
+        referer: context.req.headers.referer,
+        query: context.params.query,
+    })
+    const user = await authController.verify(context.req.headers.auth)
+    return user
 }
