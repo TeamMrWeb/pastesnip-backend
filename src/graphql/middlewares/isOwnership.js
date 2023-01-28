d662dfd739cb37f40c6ed1be8e35e2c4aae88a61
Manu <mcabral@frba.utn.edu.ar>
2023-01-28 17:01:16 -0300

    fix: remove comment

--- src/graphql/middlewares/isOwnership.js
+++ src/graphql/middlewares/isOwnership.js
@@ -1,5 +1,4 @@
 module.exports = (resolver) => async (parent, args, context, info) => {
     const { user } = context
-    console.log('catched ', user, ' in isOwnership middleware')
     return resolver(parent, args, context, info)
 }
