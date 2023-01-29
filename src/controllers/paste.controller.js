982749fbd8c1a1ca9594d9ba9912d5030cb474c4
Manu <mcabral@frba.utn.edu.ar>
2023-01-29 16:02:56 -0300

    refactor: rename repeated words

--- src/controllers/paste.controller.js
+++ src/controllers/paste.controller.js
@@ -2,12 +2,12 @@ const pasteService = require('../services/paste.service')
 const errorObject = require('../utils/error')
 
 module.exports = {
-    newPaste: async (paste) => {
+    create: async (paste_payload) => {
         try {
-            const paste = await pasteService.create(paste)
+            const paste = await pasteService.create(paste_payload)
             return paste
         } catch (error) {
             throw new errorObject({ message: error.message })
         }
-    }
+    },
 }
