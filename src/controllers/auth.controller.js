6b87d32a6016a31bf4f14b30f321b48456b52b2e
Manu <mcabral@frba.utn.edu.ar>
2023-01-28 21:57:19 -0300

    feat(controller): add logout and refresh user

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -10,6 +10,13 @@ module.exports = {
             throw new errorObject({ message: error.message })
         }
     },
+    logout: async (token) => {
+        try {
+            return await authService.logout(token)
+        } catch (error) {
+            throw new errorObject({ message: error.message })
+        }
+    },
     verify: async (token) => {
         try {
             const response = await authService.verify(token)
@@ -17,6 +24,15 @@ module.exports = {
             return response
         } catch (error) {
             throw new errorObject({ message: error.message, auth: true })
+        }
+    },
+    refresh: async (token) => {
+        try {
+            if (!token)
+                throw new errorObject({ message: 'Refresh token not provided' })
+            return await authService.refresh(token)
+        } catch (error) {
+            throw new errorObject({ message: error.message })
         }
     },
 }
