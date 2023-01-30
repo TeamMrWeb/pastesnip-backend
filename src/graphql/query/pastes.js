c596bb1e15a45e8e01696d7f77937ff2c94d194e
leoarayav <leandro.araya08@inacapmail.cl>
2023-01-30 16:35:17 -0300

    refactor: args are unnecessary.

--- src/graphql/query/pastes.js
+++ src/graphql/query/pastes.js
@@ -2,6 +2,6 @@ const pasteController = require('../../controllers/paste.controller')
 
 module.exports = async (parent, args, context) => {
     const { user } = context
-    const pastes = await pasteController.getAll({ ...args, author: user.id })
+    const pastes = await pasteController.getAll({ author: user.id })
     return pastes
 }
