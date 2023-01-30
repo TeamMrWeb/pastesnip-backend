c2e6190c7cb56d4a98260d056b0221260d5f4d24
leoarayav <leandro.araya08@inacapmail.cl>
2023-01-30 16:47:53 -0300

    refactor (query): get pastes improve

--- src/graphql/query/pastes.js
+++ src/graphql/query/pastes.js
@@ -1,6 +1,5 @@
 const pasteController = require('../../controllers/paste.controller')
 
 module.exports = async (parent, args, context) => {
-    const pastes = await pasteController.getAll()
-    return pastes
+    return await pasteController.getAll()
 }
