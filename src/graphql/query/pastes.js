f7e1a71494e385cd7890bbc755371caa0807641f
leoarayav <leandro.araya08@inacapmail.cl>
2023-01-30 16:42:21 -0300

    refactor: unused arguments removed.

--- src/graphql/query/pastes.js
+++ src/graphql/query/pastes.js
@@ -1,7 +1,6 @@
 const pasteController = require('../../controllers/paste.controller')
 
 module.exports = async (parent, args, context) => {
-    const { user } = context
-    const pastes = await pasteController.getAll({ author: user.id })
+    const pastes = await pasteController.getAll()
     return pastes
 }
