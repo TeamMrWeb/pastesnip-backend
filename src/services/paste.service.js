6ce2e3c1b279766a102912834d1c9d5391a473ed
Manu <mcabral@frba.utn.edu.ar>
2023-01-30 03:06:49 -0300

    feat(service): add delete paste

--- src/services/paste.service.js
+++ src/services/paste.service.js
@@ -6,8 +6,9 @@ module.exports = {
         const newPaste = await Paste.create(paste_payload)
         return newPaste.populate('author')
     },
+    delete: async (id) => await Paste.findByIdAndDelete(id),
     find: async (options) => await Paste.findOne(options),
     findAll: async () => await Paste.find(),
     findById: async (id) => await Paste.findById(id),
-    findByAuthor: async (author) => module.exports.find({ author })
+    findByAuthor: async (author) => module.exports.find({ author }),
 }
