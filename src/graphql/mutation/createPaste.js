d875e4e30d42fb86f5d7ab87b04961b7661820a6
leoarayav <leandro.araya08@inacapmail.cl>
2023-01-29 21:27:17 -0300

    feat(mutation): createPaste added and fixed.

--- src/graphql/mutation/createPaste.js
+++ src/graphql/mutation/createPaste.js
@@ -1,6 +1,7 @@
 const pasteController = require('../../controllers/paste.controller')
 
 module.exports = async (parent, args, context) => {
-    const paste = await pasteController.create(args)
+    const { user } = context
+    const paste = await pasteController.create({ ...args, author: user.id })
     return paste
 }
--- src/services/paste.service.js
+++ src/services/paste.service.js
@@ -2,5 +2,8 @@ const { models } = require('../database')
 const { Paste } = models
 
 module.exports = {
-    create: async (paste_payload) => await Paste.create(paste_payload),
+    create: async (paste_payload) => {
+        const newPaste = await Paste.create(paste_payload)
+        return newPaste.populate('author')
+    }
 }
