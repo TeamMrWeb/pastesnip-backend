400448f48322a38d7eba463d652b85cdc9fe7d9d
Manu <mcabral@frba.utn.edu.ar>
2023-02-02 21:20:54 -0300

    feat(middleware): checks user is verified

--- src/graphql/middlewares/isLogged.js
+++ src/graphql/middlewares/isLogged.js
@@ -1,7 +1,19 @@
 const errorObject = require('../../utils/error')
 
-module.exports = (resolver) => async (parent, args, context, info) => {
-    const { user } = context
-    if (!user) throw new errorObject({ auth: true })
-    return resolver(parent, args, context, info)
-}
+module.exports =
+    (
+        resolver,
+        options = {
+            verified: null,
+        },
+    ) =>
+    async (parent, args, context, info) => {
+        const { user } = context
+        if (!user) throw new errorObject({ auth: true })
+        if (options.verified && user.verified !== options.verified)
+            throw new errorObject({
+                message: 'User not verified to perform this operation',
+                auth: true,
+            })
+        return resolver(parent, args, context, info)
+    }
--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -5,17 +5,29 @@ module.exports = {
         me: isLogged(require('./query/me')),
         hello: require('./query/hello'),
         users: isLogged(require('./query/users')),
-        getUserById: isLogged(require('./query/getUserById')),
-        pastes: isLogged(require('./query/pastes')),
+        getUserById: isLogged(require('./query/getUserById'), {
+            verified: true,
+        }),
+        pastes: isLogged(require('./query/pastes'), { verified: true }),
     },
     Mutation: {
-        createNewUser: require('./mutation/createNewUser'),
-        deleteUser: isLogged(require('./mutation/deleteUser')),
-        updateUser: isLogged(require('./mutation/updateUser')),
+        createNewUser: isLogged(require('./mutation/createNewUser'), {
+            verified: true,
+        }),
+        deleteUser: isLogged(require('./mutation/deleteUser'), {
+            verified: true,
+        }),
+        updateUser: isLogged(require('./mutation/updateUser'), {
+            verified: true,
+        }),
+        createPaste: isLogged(require('./mutation/createPaste'), {
+            verified: true,
+        }),
+        deletePaste: isLogged(require('./mutation/deletePaste'), {
+            verified: true,
+        }),
         loginUser: require('./mutation/loginUser'),
         logoutUser: isLogged(require('./mutation/logoutUser')),
         refreshUser: require('./mutation/refreshUser'),
-        createPaste: isLogged(require('./mutation/createPaste')),
-        deletePaste: isLogged(require('./mutation/deletePaste')),
     },
 }
